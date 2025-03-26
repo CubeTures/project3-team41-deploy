69fa29065ceea37e8d9befd3cb5e99e52fa28347
Owen Shadburne <91618801+CubeTures@users.noreply.github.com>
2025-03-26 13:30:26 -0500

    port for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import "dotenv/config";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,5 +16,8 @@ export default defineConfig({
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
+	},
+	server: {
+		port: parseInt(process.env.PORT as string) || 5173,
 	},
 });
